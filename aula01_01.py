ae88fc04b4d66cf4422c8ef67f77d7a7fc5f1bf3
rafaelmds29 <rafaelmds2992@gmail.com>
2024-11-13 18:38:39 -0300

    Add files via upload

---
+++ aula01_01.py
@@ -0,0 +1,5 @@
+n1 = 10
+n2 = 5.6
+nome = 'Rafael'
+idade = 32
+print('Sr(a) ', nome, ', a sua idade é ', idade)
---
+++ aula01_02.py
@@ -0,0 +1,6 @@
+num = int(input('Informe um valor: '))
+result = num * 2
+print('O dobro de ' , num,'é igual a ', result)
+
+##numero = int(input('Qual é o dobro do número:'))
+##print(numero * 2)
